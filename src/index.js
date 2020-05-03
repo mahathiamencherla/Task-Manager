56bb54afc3fbb7065c6e555df4458716f29863bc
mahathiamencherla15 <mahathiamencherla15@gmail.com>
2020-05-03 16:35:51 +0530

    Adding authentication

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+
+// app.use((req, res, next) => {
+//     if (req.method == 'GET' ) {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//         res.status(503).send('Site under maintenance! Check back later.')
+// })
+
 app.use(express.json())
 
 app.use(userRouter)
@@ -15,3 +28,14 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log("Server is on port: " + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    const user = await User.findById('')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 const User = require('../models/user')
 
 
@@ -11,39 +12,52 @@ router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
+        const token = await user.generateAuthToken()
         await user.save()
-        res.status(201).send(user)        
+        res.status(201).send({user , token })        
     } catch (e) {
         res.status(400).send(e)        
     }
 
 })
 
-router.get('/users', async (req,res) => {
-
+router.post('/users/login', async (req, res) => {
     try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (error) {
-        res.status(500).send(error)        
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)       
     }
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
     } catch (error) {
-        res.status(500).send()        
+        res.status(500).send()
     }
 })
 
-router.patch('/users/:id', async (req, res) => {
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req,res) => {
+    res.send(req.user)
+})
+
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email','password','age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -53,25 +67,22 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+
+        await req.user.save()
+
+        res.send(req.user)
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
-router.delete('/users/:id', async (req,res) => {
+router.delete('/users/me', auth, async (req,res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
+        await req.user.remove()
 
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
+        res.send(req.user)
     } catch (error) {
         res.status(500).send()
     }
